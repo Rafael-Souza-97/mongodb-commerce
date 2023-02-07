4fc0f320538da3f1b1d0c65fd240af2936923851
Rafael Souza <souza_rafael.97@outlook.com>
2023-02-07 18:31:41 -0300

    feat: req 20 - Remove the first ingredient from the Quarter Cheese Sandwich

--- challenges/desafio19.js
+++ challenges/desafio19.js
@@ -3,4 +3,4 @@ db.produtos.updateMany(
   { $pull: { ingredientes: { $in: ["cebola"] } } },
 );
 
-db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
